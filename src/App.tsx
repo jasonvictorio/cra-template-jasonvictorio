0aa8a6ae58f7698587ee65e3a3cfe43d940f4f05
Jason Victorio <jgvictorio@gmail.com>
2020-08-09 23:51:18 +1200

    add reach router

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,22 @@
 import React from 'react'
-import logo from './logo.svg'
+import { Router, Link } from '@reach/router'
+
 import { Counter } from './features/counter/Counter'
+import About from './views/About'
+import Home from './views/Home'
 import './App.scss'
 
 function App() {
   return (
     <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a className='App-link' href='https://reactjs.org/' target='_blank' rel='noopener noreferrer'>
-            React
-          </a>
-          <span>, </span>
-          <a className='App-link' href='https://redux.js.org/' target='_blank' rel='noopener noreferrer'>
-            Redux
-          </a>
-          <span>, </span>
-          <a className='App-link' href='https://redux-toolkit.js.org/' target='_blank' rel='noopener noreferrer'>
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a className='App-link' href='https://react-redux.js.org/' target='_blank' rel='noopener noreferrer'>
-            React Redux
-          </a>
-        </span>
-      </header>
+      <Counter />
+      <Router>
+        <Home path='/' />
+        <About path='/about' />
+      </Router>
+
+      <Link to='/'>Home</Link>
+      <Link to='/about'>About</Link>
     </div>
   )
 }
